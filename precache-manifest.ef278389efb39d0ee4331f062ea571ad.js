6e17eb156d11848c88b3020063feca7fb3bb3953
MohamedElkamary2 <amo263128@gmail.com>
2020-08-07 18:01:15 +0200

    Updates

---
+++ precache-manifest.ef278389efb39d0ee4331f062ea571ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8371b87829228f2e22c71934ac7c498a",
+    "url": "/crudapp/index.html"
+  },
+  {
+    "revision": "c41a7349ba2267ab8dde",
+    "url": "/crudapp/static/css/main.41438e91.chunk.css"
+  },
+  {
+    "revision": "48b77796b6a81721cb99",
+    "url": "/crudapp/static/js/2.8ba1fa5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crudapp/static/js/2.8ba1fa5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41a7349ba2267ab8dde",
+    "url": "/crudapp/static/js/main.09005727.chunk.js"
+  },
+  {
+    "revision": "59988a0910956682d578",
+    "url": "/crudapp/static/js/runtime-main.58c77b51.js"
+  }
+]);
